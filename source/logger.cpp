33b4a310dce4bc512371173b14e6939c73e561e6
carenceLiu <liuwenrui@pku.edu.cn>
2024-03-24 16:52:59 +0800

    debug

--- source/logger.cpp
+++ source/logger.cpp
@@ -1,4 +1,5 @@
 #include "logger.h"
+#include <iostream>
 using namespace std;
 
 Logger::Logger(const string log_filename, const string loss_filename): 
@@ -6,10 +7,12 @@ Logger::Logger(const string log_filename, const string loss_filename):
 
 void Logger::log(int epoch, double loss, double acc) {
     logFile << "[Train Log] epoch " << epoch << ", loss " << loss <<", prediction accuracy: " << acc << endl; 
+    cout << "[Train Log] epoch " << epoch << ", loss " << loss <<", prediction accuracy: " << acc << endl; 
 }
 
 void Logger::log(const string & line) {
     logFile << line << endl;
+    cout << line << endl;
 }
 
 void Logger::lossPrint(double loss) {
